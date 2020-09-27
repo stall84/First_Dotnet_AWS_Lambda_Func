b5d05e157d3616f0339a8a47dc060fb686559d0f
Michael Stallings <stall84@gmail.com>
2020-09-27 00:55:18 -0400

    initial commit, folder-structure created via template. newuser class created

---
+++ src/MySimpleFunc1/NewUser.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Amazon.Lambda.Core;
+
+namespace MySimpleFunc1
+{
+
+
+    public class NewUser
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
